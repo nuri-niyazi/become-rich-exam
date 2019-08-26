7141acbf20708502f209531cdf8eadf15408352a
Nuri Niyazi <nuri.n.93@gmail.com>
2019-08-26 16:33:44 +0300

    Updates

---
+++ precache-manifest.4cd76b61e05a02c9287a3d09b7f197c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0811580d96aa53d785233776e34c1b78",
+    "url": "/become-rich-exam/index.html"
+  },
+  {
+    "revision": "8ba2b8fd073699ec01f3",
+    "url": "/become-rich-exam/static/css/main.6ccd3bc4.chunk.css"
+  },
+  {
+    "revision": "392b376638486c3fff7b",
+    "url": "/become-rich-exam/static/js/2.ff47a0cb.chunk.js"
+  },
+  {
+    "revision": "8ba2b8fd073699ec01f3",
+    "url": "/become-rich-exam/static/js/main.ea80a929.chunk.js"
+  },
+  {
+    "revision": "c76d58b0f296f877d263",
+    "url": "/become-rich-exam/static/js/runtime~main.f70edc3a.js"
+  }
+]);
